b109d581705775082147b93bafd911d32a6e2bfb
Soroush <s_shemshadi@ymail.com>
2022-01-04 09:37:11 +0330

    [ADD] ZipCompressor

---
+++ src/main/java/io/github/shuoros/jcompressor/compress/ZipCompressor.java
@@ -0,0 +1,19 @@
+package io.github.shuoros.jcompressor.compress;
+
+import io.github.shuoros.jcompressor.JCompressor;
+
+import java.io.File;
+
+public class ZipCompressor implements JCompressor {
+
+    @Override
+    public void compress(File file) {
+
+    }
+
+    @Override
+    public void extract(File file) {
+
+    }
+
+}
