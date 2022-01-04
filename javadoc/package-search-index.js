93d3f7e0a21336453f3a3cd58baebaf99e18b185
Soroush <s_shemshadi@ymail.com>
2022-01-04 19:27:40 +0330

    [ADD] javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.github.shuoros.jcompressor"},{"l":"io.github.shuoros.jcompressor.compress"},{"l":"io.github.shuoros.jcompressor.exception"}]
