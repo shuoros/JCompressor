003d77bd34cba5df617cab280812ecab640360e4
Soroush <s_shemshadi@ymail.com>
2022-01-17 00:45:20 +0330

    [ADD] GZipCompressor class

---
+++ src/main/java/io/github/shuoros/jcompressor/compress/GZipCompressor.java
@@ -0,0 +1,20 @@
+package io.github.shuoros.jcompressor.compress;
+
+import io.github.shuoros.jcompressor.JCompressor;
+
+import java.io.File;
+import java.util.List;
+
+public class GZipCompressor implements JCompressor {
+
+    @Override
+    public void compress(List<File> files, File destinationFile) {
+
+    }
+
+    @Override
+    public void extract(File compressedFile, File destinationFile) {
+
+    }
+    
+}
